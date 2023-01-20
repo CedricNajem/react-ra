e40d325ca1e060a14ae849aa895a0a63d36a9704
CRC <92426463+CedricNajem@users.noreply.github.com>
2023-01-20 15:18:46 +0100

    commit avancement

--- rat/src/App.js
+++ rat/src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Router>
+          <Switch>
+            <Route path="/page1" component={Page1} />
+            <Route path="/page2" component={Page2} />
+            <Route path="/page3" component={Page3} />
+          </Switch>
+        </Router>
+        <nav>
+          <Link to="/page1">Page 1</Link>
+          <Link to="/page2">Page 2</Link>
+          <Link to="/page3">Page 3</Link>
+        </nav>
       </header>
     </div>
   );
